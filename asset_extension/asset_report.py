4cc6c7b1b86118e9b92267c4cf7b0883a4d486d4
Riyaj Pathan <rjpathan19@gmail.com>
2015-10-26 19:21:48 +0530

    show Total Assets in Asset list Report

--- asset_extension/asset_report.py
+++ asset_extension/asset_report.py
@@ -8,6 +8,7 @@ class asset_list_report(osv.osv):
 		'name': fields.char('Name'),
 		'partner_id': fields.many2one('res.partner', 'Partner'),
 		'asset_list': fields.one2many('asset.asset', 'asset_report_id', 'Assets'),
+		'total_assets': fields.integer('Total'),
 	}
 
 	_defaults = {
@@ -24,6 +25,7 @@ class asset_list_report(osv.osv):
 				cr.execute("update asset_asset set asset_report_id=%s where id=%s"%(ids[0], assets[0]))
 			elif len(assets) and len(assets) > 1:
 				cr.execute("update asset_asset set asset_report_id=%s where id in %s"%(ids[0], tuple(assets)))
+			self.write(cr, uid, ids, {'total_assets': len(assets)})
 		return self.pool['report'].get_action(cr, uid, ids, 'asset_extension.report_assetlist', context=context)
 
 class asset_asset(osv.osv):
