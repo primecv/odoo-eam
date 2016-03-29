42b1767f7809ef5e4fe3525de8bb8b6142eea5d0
Riyaj Pathan <rjpathan19@gmail.com>
2016-03-29 07:52:51 +0530

    Module for Customizing Odoo Base Module

---
+++ base_hcv/__openerp__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2015 Prime Consulting, Cape Verde (<http://prime.cv>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'Base Extension for HCV',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Base Extension for HCV - General Customization of Base Modules',
+    'author': 'Prime Consulting, Cape Verde',
+    'website': 'prime.cv',
+    'category': 'Tools',
+    'depends': ['base', 'purchase'],
+    'data': [
+        'partner_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
