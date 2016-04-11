9d5d72751f048dc0d43ae64f461e018ef7d59215
Riyaj Pathan <rjpathan19@gmail.com>
2016-04-11 16:56:51 +0530

    add total assets field; add partner field for report translation

--- asset_extension/wizard/asset_list.py
+++ asset_extension/wizard/asset_list.py
@@ -8,6 +8,12 @@ class hcv_equipment_list(osv.osv_memory):
 	_columns = {
 		'location_id': fields.many2one('stock.location', 'Location', domain="[('usage','=','asset')]"),
 		'asset_ids': fields.one2many('asset.asset.hcv.list', 'hcv_id', 'Assets'),
+		'total_assets': fields.integer('Total Assets'),
+		'partner_id': fields.many2one('res.partner', 'Partner'),
+	}
+
+	_defaults = {
+		'partner_id': lambda self, cr, uid, context: self.pool.get('res.users').browse(cr, uid, [uid])[0].partner_id.id
 	}
 
 	def print_report(self, cr, uid, ids, context=None):
@@ -16,7 +22,8 @@ class hcv_equipment_list(osv.osv_memory):
 			assets = self.pool.get('asset.asset').search(cr, uid, [('property_stock_asset','=',loc_id)])
 			for asset in assets:
 				self.pool.get('asset.asset.hcv.list').create(cr, uid, {'hcv_id': rec.id, 'asset_id': asset})
-		return True
+			self.write(cr, uid, ids, {'total_assets': len(assets)})
+		return self.pool['report'].get_action(cr, uid, ids, 'asset_extension.report_hcv_equipments', context=context)
 
 
 class asset_asset_hcv_list(osv.osv_memory):
