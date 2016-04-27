e7c40b1cdaee123a4668e0942c5ffd48e921ed6d
Riyaj Pathan <rjpathan19@gmail.com>
2016-04-27 17:51:40 +0530

    Generate Depreciation lines from Purchase Date for Linear Type

--- asset_account_hcv_extension/asset.py
+++ asset_account_hcv_extension/asset.py
@@ -20,6 +20,9 @@
 ##############################################################################
 
 from openerp.osv import fields, osv
+import time
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 class asset_asset(osv.osv):
 	_inherit = "asset.asset"
@@ -90,6 +93,43 @@ class account_asset(osv.osv):
 					raise osv.except_osv(('Validation Error!'), ("Invalid Depreciation Date Range."))
 		return super(account_asset, self).validate(cr, uid, ids, context)
 
+	def _compute_board_undone_dotation_nb(self, cr, uid, asset, depreciation_date, total_days, context=None):
+		undone_dotation_number = asset.method_number
+		if asset.method_time == 'end':
+			end_date = datetime.strptime(asset.method_end, '%Y-%m-%d')
+			undone_dotation_number = 0
+			while depreciation_date <= end_date:
+				depreciation_date = (datetime(depreciation_date.year, depreciation_date.month, depreciation_date.day) + relativedelta(months=+asset.method_period))
+				undone_dotation_number += 1
+		if asset.prorata:
+			undone_dotation_number += 1
+		return undone_dotation_number
+
+	def _compute_board_amount(self, cr, uid, asset, i, residual_amount, amount_to_depr, undone_dotation_number, posted_depreciation_line_ids, total_days, depreciation_date, context=None):
+		#by default amount = 0
+		amount = 0
+		if i == undone_dotation_number:
+			amount = residual_amount
+		else:
+			if asset.method == 'linear':
+				amount = amount_to_depr / (undone_dotation_number - len(posted_depreciation_line_ids))
+				if asset.prorata:
+					amount = amount_to_depr / asset.method_number
+					days = total_days - float(depreciation_date.strftime('%j'))
+					if i == 1:
+						amount = (amount_to_depr / asset.method_number) / total_days * days
+					elif i == undone_dotation_number:
+						amount = (amount_to_depr / asset.method_number) / total_days * (total_days - days)
+			elif asset.method == 'degressive':
+				amount = residual_amount * asset.method_progress_factor
+				if asset.prorata:
+					days = total_days - float(depreciation_date.strftime('%j'))
+					if i == 1:
+						amount = (residual_amount * asset.method_progress_factor) / total_days * days
+					elif i == undone_dotation_number:
+						amount = (residual_amount * asset.method_progress_factor) / total_days * (total_days - days)
+		return amount
+
 	def compute_depreciation_board(self, cr, uid, ids, context=None):
 		depreciation_line_obj = self.pool.get('account.asset.depreciation.line')
 		currency_obj = self.pool.get('res.currency')
@@ -136,7 +176,58 @@ class account_asset(osv.osv):
 																	})
 					total_depreciation = total_depreciation + current_depreciation
 			else:
-				return super(account_asset, self).compute_depreciation_board(cr, uid, ids, context)
+				depreciation_lin_obj = self.pool.get('account.asset.depreciation.line')
+				currency_obj = self.pool.get('res.currency')
+				for asset in self.browse(cr, uid, ids, context=context):
+					if asset.value_residual == 0.0:
+						continue
+					posted_depreciation_line_ids = depreciation_lin_obj.search(cr, uid, [('asset_id', '=', asset.id), ('move_check', '=', True)],order='depreciation_date desc')
+					old_depreciation_line_ids = depreciation_lin_obj.search(cr, uid, [('asset_id', '=', asset.id), ('move_id', '=', False)])
+					if old_depreciation_line_ids:
+						depreciation_lin_obj.unlink(cr, uid, old_depreciation_line_ids, context=context)
+
+					amount_to_depr = residual_amount = asset.value_residual
+					if asset.prorata:
+						depreciation_date = datetime.strptime(self._get_last_depreciation_date(cr, uid, [asset.id], context)[asset.id], '%Y-%m-%d')
+					else:
+						# depreciation_date = 1st January of purchase year
+						purchase_date = datetime.strptime(asset.purchase_date, '%Y-%m-%d')
+						#if we already have some previous validated entries, starting date isn't 1st January but last entry + method period
+						if (len(posted_depreciation_line_ids)>0):
+							last_depreciation_date = datetime.strptime(depreciation_lin_obj.browse(cr,uid,posted_depreciation_line_ids[0],context=context).depreciation_date, '%Y-%m-%d')
+							depreciation_date = (last_depreciation_date+relativedelta(months=+asset.method_period))
+						else:
+							depreciation_date = datetime(purchase_date.year, purchase_date.month, purchase_date.day)
+					day = depreciation_date.day
+					month = depreciation_date.month
+					year = depreciation_date.year
+					total_days = (year % 4) and 365 or 366
+
+					undone_dotation_number = self._compute_board_undone_dotation_nb(cr, uid, asset, depreciation_date, total_days, context=context)
+					for x in range(len(posted_depreciation_line_ids), undone_dotation_number):
+						i = x + 1
+						amount = self._compute_board_amount(cr, uid, asset, i, residual_amount, amount_to_depr, undone_dotation_number, posted_depreciation_line_ids, total_days, depreciation_date, context=context)
+						company_currency = asset.company_id.currency_id.id
+						current_currency = asset.currency_id.id
+						# compute amount into company currency
+						amount = currency_obj.compute(cr, uid, current_currency, company_currency, amount, context=context)
+						residual_amount -= amount
+						vals = {
+							 'amount': amount,
+							 'asset_id': asset.id,
+							 'sequence': i,
+							 'name': str(asset.id) +'/' + str(i),
+							 'remaining_value': residual_amount,
+							 'depreciated_value': (asset.purchase_value - asset.salvage_value) - (residual_amount + amount),
+							 'depreciation_date': depreciation_date.strftime('%Y-%m-%d'),
+						}
+						depreciation_lin_obj.create(cr, uid, vals, context=context)
+						# Considering Depr. Period as months
+						depreciation_date = (datetime(year, month, day) + relativedelta(months=+asset.method_period))
+						day = depreciation_date.day
+						month = depreciation_date.month
+						year = depreciation_date.year
+				return True
 
 class account_asset_depreciation_line(osv.osv):
 	_inherit = "account.asset.depreciation.line"
